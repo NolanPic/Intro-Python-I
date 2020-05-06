a00890b6713a76376ede8d1600c727222f710dd8
Nolan Picini <nolanpic@gmail.com>
2020-05-05 19:24:57 -0700

    Remove unnecessary int cast

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -39,7 +39,7 @@ print('\n***********\n')
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = [int(i) for i in x if (int(i) % 2) == 0]
+y = [i for i in x if (int(i) % 2) == 0]
 
 print(y)
 print('\n***********\n')
